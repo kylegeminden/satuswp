2f2b5a51f4661f914e3145d8bf9e128bb2718f52
Kyle Geminden <kyle@kylegeminden.com>
2012-11-29 19:41:45 -0700

    Updated based on Roots Theme

--- templates/page-header.php
+++ templates/page-header.php
@@ -1,5 +1,5 @@
 <div class="page-header">
-  <h1 property="headline">
+  <h1 property="headline">    
     <?php
       if (is_home()) {
         if (get_option('page_for_posts', true)) {
@@ -7,7 +7,7 @@
         } else {
           _e('Blog', 'satus');
         }
-      } elseif ( is_archive() && !is_author() && !is_category() && !is_tag() ) {
+      } elseif (is_archive()) {
         $term = get_term_by('slug', get_query_var('term'), get_query_var('taxonomy'));
         if ($term) {
           echo $term->name;
@@ -19,17 +19,15 @@
           printf(__('Monthly Archives: %s', 'satus'), get_the_date('F Y'));
         } elseif (is_year()) {
           printf(__('Yearly Archives: %s', 'satus'), get_the_date('Y'));
+        } elseif (is_author()) {
+          global $post;
+          $author_id = $post->post_author;
+          printf(__('About: %s', 'satus'), get_the_author_meta('display_name', $author_id));
+        } else {
+          single_cat_title();
         }
-      } elseif (is_category()) {
-        printf( __( '%s', 'satus' ), single_cat_title( '', false ) );
-      } elseif (is_tag()) {
-        printf( __( '%s', 'satus' ), single_tag_title( '', false ) );
-      } elseif (is_author()) {
-        global $post;
-        $author_id=$post->post_author; 
-        printf(__('<span>About:</span> %s', 'satus'), get_the_author_meta('display_name', $author_id));
       } elseif (is_search()) {
-        printf( __( 'Search Results for &lsquo;%s&rsquo;', 'satus' ), get_search_query() );
+        printf(__('Search Results for &lsquo;%s&rsquo;', 'satus'), get_search_query());
       } elseif (is_404()) {
         _e('Whoa!', 'satus');
       } else {
