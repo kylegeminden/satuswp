0bf717bbad9b1ad1a0df4afecf8d341bb10e2909
Kyle Geminden <kyle@kylegeminden.com>
2012-12-11 17:49:39 -0700

    Updated feed link
    Removed post type check as it wasn't needed.

--- templates/head.php
+++ templates/head.php
@@ -24,8 +24,8 @@
 
   <?php wp_head(); ?>
 
-  <?php if (wp_count_posts()->publish > 0 && get_post_type() !== 'page') : ?>
-  <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo home_url() ?>/feed/">
+  <?php if (wp_count_posts()->publish > 0) : ?>
+  <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo home_url(); ?>/feed/">
   <?php endif; ?>
   
 </head>
