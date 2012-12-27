5187f1507f432d8eaf48bbccf046b41a586e33ab
Kyle Geminden <kyle@kylegeminden.com>
2012-12-27 11:51:42 -0700

    Fixed error in call for the page header as it was the wrong file name.

--- templates-page/page-full.php
+++ templates-page/page-full.php
@@ -4,7 +4,7 @@ Template Name: Full Width
 */
 ?>
 
-<?php get_template_part( 'templates/content', 'header' ); ?>
+<?php get_template_part( 'templates/page-header' ); ?>
 
 <?php while ( have_posts() ) : the_post(); ?>
 <?php get_template_part( 'templates/content', 'page' ); ?>
