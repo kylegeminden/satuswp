b8ba1e2cc5c7fd2bc2708d1de604cf34fbda9d2c
Kyle Geminden <kyle@kylegeminden.com>
2012-12-27 10:39:14 -0700

    Removed commented out section for setting default thumbnail size as it can be set in the admin area.

--- inc/custom.php
+++ inc/custom.php
@@ -1,7 +1,5 @@
 <?php
 
-// set_post_thumbnail_size( 100, 100, true ); // 100 pixels wide by 100 pixels tall, crop mode
-// additional image sizes
 // add_image_size( 'custom_name', 700, 300, true ); //700 pixels wide by 300 tall
 
 // Add post formats (http://codex.wordpress.org/Post_Formats)
