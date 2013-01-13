97a3052b80f73834b43493f185e514469fbaf233
Kyle Geminden <kyle@kylegeminden.com>
2013-01-12 18:57:53 -0700

    Updated structure for grid.

--- templates/footer.php
+++ templates/footer.php
@@ -1,30 +1,32 @@
 <footer id="footer" role="contentinfo">
-  <section class="row expand">
+  <section class="row">
+    <div class="column width1of1">
 
-    <h2 class="assistive-text"><?php _e( 'Footer', 'satus' ); ?></h2>
-    
-    <section class="org-info" vocab="http://schema.org/" typeof="Organization">
-      <h3 property="name">
-        <?php bloginfo('name'); ?>
-      </h3>
-      <p>
-        <span property="address" vocab="http://schema.org/" typeof="http://schema.org/PostalAddress">
-          <span property="streetAddress">Street Address</span><br>
-          <span property="addressLocality">City</span>,
-          <span property="addressRegion">State</span>
-          <span property="postalCode">Zip Code</span><br>
-          <span property="addressCountry">Country</span>
-        </span><br>
-        <span>Phone</span>: <span property="telephone"><a class="telephone" href="tel:155555555555">1-555-555-5555</a></span><br>
-        <span>Fax</span>: <span property="faxNumber"><a class="telephone" href="tel:155555555555">1-555-555-5555</a></span><br>
-        <span><a href="<?php echo home_url('/'); ?>" property="url" rel="bookmark">website.com</a></span><br>
-        <span><a href="mailto:you@yourdomain.com" property="email">you@yourdomain.com</a></span>
-      </p>
-    </section>
-    
-    <p class="copyright"><small>&copy; <?php echo date('Y');?> <?php bloginfo('name'); ?></small></p>
-    <p class="design-by">design by <a href="#">Your Design Company</a></p>
-    
+      <h2 class="assistive-text"><?php _e( 'Footer', 'satus' ); ?></h2>
+      
+      <section class="org-info" vocab="http://schema.org/" typeof="Organization">
+        <h3 property="name">
+          <?php bloginfo('name'); ?>
+        </h3>
+        <p>
+          <span property="address" vocab="http://schema.org/" typeof="http://schema.org/PostalAddress">
+            <span property="streetAddress">Street Address</span><br>
+            <span property="addressLocality">City</span>,
+            <span property="addressRegion">State</span>
+            <span property="postalCode">Zip Code</span><br>
+            <span property="addressCountry">Country</span>
+          </span><br>
+          <span>Phone</span>: <span property="telephone"><a class="telephone" href="tel:155555555555">1-555-555-5555</a></span><br>
+          <span>Fax</span>: <span property="faxNumber"><a class="telephone" href="tel:155555555555">1-555-555-5555</a></span><br>
+          <span><a href="<?php echo home_url('/'); ?>" property="url" rel="bookmark">website.com</a></span><br>
+          <span><a href="mailto:you@yourdomain.com" property="email">you@yourdomain.com</a></span>
+        </p>
+      </section>
+      
+      <p class="copyright"><small>&copy; <?php echo date('Y');?> <?php bloginfo('name'); ?></small></p>
+      <p class="design-by">design by <a href="#">Your Design Company</a></p>
+
+    </div>
   </section>
 </footer>
 <!-- /#footer -->
--- templates/header.php
+++ templates/header.php
@@ -1,29 +1,31 @@
 <header id="header" role="banner">  
-  <div class="row expand">
-    
-    <hgroup id="brand">
-      <h1 id="site-title">
-        <a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
-      </h1>
-      <h2 id="site-description"><?php bloginfo( 'description' ); ?></h2>
-    </hgroup>
-    
-    <nav id="primary-nav" role="navigation">
-      <h2 class="assistive-text"><?php _e( 'Primary Navigation', 'satus' ); ?></h2>
-      <a class="assistive-text" href="#main" title="<?php _e('Go to main content', 'satus') ?>"><?php _e('Go to main content', 'satus') ?></a>
-      <?php
-        if (has_nav_menu('primary_nav')) {
-          wp_nav_menu(array(
-            'theme_location' => 'primary_nav',
-            'depth' => 2,
-            'container' => false,
-            'items_wrap' => '<ul class="nav">%3$s</ul>',
-            'walker' => new Satus_Navbar_Walker()
-          ));
-        }
-      ?>
-    </nav>
+  <div class="row">
+    <div class="column width1of1">
+      
+      <hgroup id="brand">
+        <h1 id="site-title">
+          <a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+        </h1>
+        <h2 id="site-description"><?php bloginfo( 'description' ); ?></h2>
+      </hgroup>
+      
+      <nav id="primary-nav" role="navigation">
+        <h2 class="assistive-text"><?php _e( 'Primary Navigation', 'satus' ); ?></h2>
+        <a class="assistive-text" href="#main" title="<?php _e('Go to main content', 'satus') ?>"><?php _e('Go to main content', 'satus') ?></a>
+        <?php
+          if (has_nav_menu('primary_nav')) {
+            wp_nav_menu(array(
+              'theme_location' => 'primary_nav',
+              'depth' => 2,
+              'container' => false,
+              'items_wrap' => '<ul class="nav">%3$s</ul>',
+              'walker' => new Satus_Navbar_Walker()
+            ));
+          }
+        ?>
+      </nav>
 
+    </div>
   </div>
 </header>
 <!-- /#header -->
