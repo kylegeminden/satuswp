d5dd27b7874f9cdd8aad4f4def9fb65b14ccac88
Kyle Geminden <kyle@kylegeminden.com>
2013-02-05 19:28:52 -0700

    Placed back custom not i.e. conditional, replaced loading satus.css and app.css with main.css and main-ie.css.

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -1,5 +1,28 @@
 <?php
 
+/**
+ * Modified from @link gist.github.com/2567027
+ * Add support for non IE conditional comments
+ *
+ * Usage:
+ * After registering or enqueuing a script you need to append the CC to the style
+ * handle via the $wp_styles class:
+ *
+ * $GLOBALS['wp_styles']->add_data( 'my-handle', 'notie_conditional', 'gte IE 9' );
+ */
+
+add_filter( 'style_loader_tag', 'style_loader_tag_ccs', 10, 2 );
+function style_loader_tag_ccs( $tag, $handle ) {
+  global $wp_styles;
+  $obj = $wp_styles->registered[ $handle ];
+  if ( isset( $obj->extra[ 'notie_conditional' ] ) && $obj->extra[ 'notie_conditional' ] ) {
+    $cc = "<!--[if {$obj->extra['notie_conditional']}]><!-->\n";
+    $end_cc = "<!--<![endif]-->\n";
+    $tag = $cc . $tag . $end_cc;
+  }
+  return $tag;
+}
+
 /** 
  * @link wp.smashingmagazine.com/2011/10/12/developers-guide-conflict-free-javascript-css-wordpress/
 */
@@ -9,16 +32,26 @@
 */
 function satus_register_styles(){
   wp_register_style(
-    'satus', //handle
-    get_stylesheet_directory_uri().'/assets/css/satus.css', //source
+    'satus-main-css', //handle
+    get_stylesheet_directory_uri().'/assets/css/main.css', //source
     null, // dependencies
-    null  // version
+    null // version
   );
   wp_register_style(
-    'satus-app',
-    get_stylesheet_directory_uri().'/assets/css/app.css',
+    'satus-main-css-ie',
+    get_stylesheet_directory_uri().'/assets/css/main-ie.css',
     null,
     null
+  );
+  $GLOBALS['wp_styles']->add_data(
+    'satus-main-css',
+    'notie_conditional', // is a custom non-wp conditional comment
+    'gte IE 9' // the conditional comment
+  ); 
+  $GLOBALS['wp_styles']->add_data(
+    'satus-main-css-ie',
+    'conditional', // is a conditional comment
+    'IE 8'
   );
 }
 add_action('init', 'satus_register_styles');
@@ -28,8 +61,8 @@ add_action('init', 'satus_register_styles');
 */
 function satus_enqueue_styles(){
   if (!is_admin()):
-    wp_enqueue_style('satus');
-    wp_enqueue_style('satus-app');
+    wp_enqueue_style('satus-main-css');
+    wp_enqueue_style('satus-main-css-ie');
   endif;
 }
 add_action('wp_enqueue_scripts', 'satus_enqueue_styles', 100);
