b9ad85d31a2392f7534b8dfb903ce48e28e539a5
Kyle Geminden <kyle@kylegeminden.com>
2013-02-10 09:09:18 -0700

    Updates transferred from Satus HTML 2.0.0.

--- templates-page/page-elements.php
+++ templates-page/page-elements.php
@@ -16,7 +16,7 @@ Template Name: Elements
 <h5>Heading 5</h5> 
 <h6>Heading 6</h6> 
 
-<p>Lorem ipsum <span class="label label-info">dolor</span> sit amet, consectetur adipiscing elit. Cras scelerisque tristique condimentum. Nulla facilisi. Proin sed velit arcu, at mattis nibh. Nam rhoncus sem quis erat mollis aliquam. Suspendisse vel mauris nunc, a luctus risus.
+<p><strong>Add class <code>.highlight</code> to highlight text</strong>. <span class="highlight">Lorem ipsum dolor sit amet</span>, consectetur adipiscing elit. Cras scelerisque tristique condimentum. Nulla facilisi. Proin sed velit arcu, at mattis nibh. Nam rhoncus sem quis erat mollis aliquam. Suspendisse vel mauris nunc, a luctus risus.
 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras scelerisque tristique condimentum. Nulla facilisi. Proin sed velit arcu, at mattis nibh. Nam rhoncus sem quis erat mollis aliquam. Suspendisse vel mauris nunc, a luctus risus.
 
 <h1>Heading 1</h1>
@@ -44,29 +44,29 @@ Template Name: Elements
 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras scelerisque tristique condimentum. Nulla facilisi. Proin sed velit arcu, at mattis nibh. Nam rhoncus sem quis erat mollis aliquam. Suspendisse vel mauris nunc, a luctus risus.
 
 <p> 
-The <a href="#">a element</a> example<br> 
-The <abbr>abbr element</abbr> and <abbr title="Title text">abbr element with title</abbr> examples<br> 
-The <b>b element</b> example<br> 
-The <cite>cite element</cite> example<br> 
-The <code>code element</code> example<br> 
-The <del>del element</del> example<br> 
-The <dfn>dfn element</dfn> and <dfn title="Title text">dfn element with title</dfn> examples<br> 
-The <em>em element</em> example<br> 
-The <i>i element</i> example<br> 
-The <ins>ins element</ins> example<br> 
-The <kbd>kbd element</kbd> example<br> 
-The <mark>mark element</mark> example<br> 
-The <q>q element <q>inside</q> a q element</q> example<br> 
-The <s>s element</s> example<br> 
-The <samp>samp element</samp> example<br> 
-The <small>small element</small> example<br> 
-The <span>span element</span> example<br> 
-The <strike>strike element</strike> example<br> 
-The <strong>strong element</strong> example<br> 
-The <sub>sub element</sub> example<br> 
-The <sup>sup element</sup> example<br> 
-The <var>var element</var> example<br> 
-The <u>u element</u> example
+  The <a href="#">a element</a> example<br> 
+  The <abbr>abbr element</abbr> and <abbr title="Title text">abbr element with title</abbr> examples<br> 
+  The <b>b element</b> example<br> 
+  The <cite>cite element</cite> example<br> 
+  The <code>code element</code> example<br> 
+  The <del>del element</del> example<br> 
+  The <dfn>dfn element</dfn> and <dfn title="Title text">dfn element with title</dfn> examples<br> 
+  The <em>em element</em> example<br> 
+  The <i>i element</i> example<br> 
+  The <ins>ins element</ins> example<br> 
+  The <kbd>kbd element</kbd> example<br> 
+  The <mark>mark element</mark> example<br> 
+  The <q>q element <q>inside</q> a q element</q> example<br> 
+  The <s>s element</s> example<br> 
+  The <samp>samp element</samp> example<br> 
+  The <small>small element</small> example<br> 
+  The <span>span element</span> example<br> 
+  The <strike>strike element</strike> example<br> 
+  The <strong>strong element</strong> example<br> 
+  The <sub>sub element</sub> example<br> 
+  The <sup>sup element</sup> example<br> 
+  The <var>var element</var> example<br> 
+  The <u>u element</u> example
 
 
 <h3>horizontal rule</h3>
@@ -74,78 +74,78 @@ The <u>u element</u> example
 
 <h3>blockquote</h3>
 <blockquote>
-<p>Some sort of famous witty quote marked up with a <code>&lt;blockquote&gt;</code> and a child <code>&lt;p&gt;</code> element.
-<p><small>Someone famous in <cite title="Source Title">Source Title</cite></small>
+  <p>Some sort of famous witty quote marked up with a <code>&lt;blockquote&gt;</code> and a child <code>&lt;p&gt;</code> element.
+  <p><small>Someone famous in <cite title="Source Title">Source Title</cite></small>
 </blockquote>
 <blockquote>
-<p>Even better philosophical quote marked up with just a <code>&lt;blockquote&gt;</code> element.
-<p><small>Someone famous in <cite title="Source Title">Source Title</cite></small>
+  <p>Even better philosophical quote marked up with just a <code>&lt;blockquote&gt;</code> element.
+  <p><small>Someone famous in <cite title="Source Title">Source Title</cite></small>
 </blockquote>
 
 <h3>unordered list</h3>
 <ul>
-<li>Aliquam eget risus erat.</li>
-<li>Aliquam eget risus erat.
-  <ul>
-    <li>Aliquam eget risus erat.</li>
-    <li>Aliquam eget risus erat.</li>
-  </ul>
-  <ol>
-    <li>Aliquam eget risus erat.</li>
-    <li>Aliquam eget risus erat.</li>
-    <li>Aliquam eget risus erat.</li>
-    <li>Aliquam eget risus erat.</li>
-    <li>Aliquam eget risus erat.</li>
-    <li>Aliquam eget risus erat.</li>
-    <li>Aliquam eget risus erat.</li>
-    <li>Aliquam eget risus erat.</li>
-    <li>Aliquam eget risus erat.</li>
-    <li>Aliquam eget risus erat.</li>
-    <li>Aliquam eget risus erat.</li>
-  </ol>
-</li>
-<li>Aliquam eget risus erat.</li>
-<li>Aliquam eget risus erat.</li>
-<li>Aliquam eget risus erat.</li>
+  <li>Aliquam eget risus erat.</li>
+  <li>Aliquam eget risus erat.
+    <ul>
+      <li>Aliquam eget risus erat.</li>
+      <li>Aliquam eget risus erat.</li>
+    </ul>
+    <ol>
+      <li>Aliquam eget risus erat.</li>
+      <li>Aliquam eget risus erat.</li>
+      <li>Aliquam eget risus erat.</li>
+      <li>Aliquam eget risus erat.</li>
+      <li>Aliquam eget risus erat.</li>
+      <li>Aliquam eget risus erat.</li>
+      <li>Aliquam eget risus erat.</li>
+      <li>Aliquam eget risus erat.</li>
+      <li>Aliquam eget risus erat.</li>
+      <li>Aliquam eget risus erat.</li>
+      <li>Aliquam eget risus erat.</li>
+    </ol>
+  </li>
+  <li>Aliquam eget risus erat.</li>
+  <li>Aliquam eget risus erat.</li>
+  <li>Aliquam eget risus erat.</li>
 </ul>
 
 <h2>ordered list</h2>
 <ol>
-<li>Aliquam eget risus erat.</li>
-<li>Aliquam eget risus erat.
-  <ul>
-    <li>Aliquam eget risus erat.</li>
-    <li>Aliquam eget risus erat.</li>
-  </ul>
-  <ol>
-    <li>Aliquam eget risus erat.</li>
-    <li>Aliquam eget risus erat.</li>
-    <li>Aliquam eget risus erat.</li>
-    <li>Aliquam eget risus erat.</li>
-    <li>Aliquam eget risus erat.</li>
-    <li>Aliquam eget risus erat.</li>
-    <li>Aliquam eget risus erat.</li>
-    <li>Aliquam eget risus erat.</li>
-    <li>Aliquam eget risus erat.</li>
-    <li>Aliquam eget risus erat.</li>
-    <li>Aliquam eget risus erat.</li>
-  </ol>
-</li>
-<li>Aliquam eget risus erat.</li>
-<li>Aliquam eget risus erat.</li>
-<li>Aliquam eget risus erat.</li>
+  <li>Aliquam eget risus erat.</li>
+  <li>Aliquam eget risus erat.
+    <ul>
+      <li>Aliquam eget risus erat.</li>
+      <li>Aliquam eget risus erat.</li>
+    </ul>
+    <ol>
+      <li>Aliquam eget risus erat.</li>
+      <li>Aliquam eget risus erat.</li>
+      <li>Aliquam eget risus erat.</li>
+      <li>Aliquam eget risus erat.</li>
+      <li>Aliquam eget risus erat.</li>
+      <li>Aliquam eget risus erat.</li>
+      <li>Aliquam eget risus erat.</li>
+      <li>Aliquam eget risus erat.</li>
+      <li>Aliquam eget risus erat.</li>
+      <li>Aliquam eget risus erat.</li>
+      <li>Aliquam eget risus erat.</li>
+    </ol>
+  </li>
+  <li>Aliquam eget risus erat.</li>
+  <li>Aliquam eget risus erat.</li>
+  <li>Aliquam eget risus erat.</li>
 </ol>
 
 <h3>definition list</h3>
 <dl>
-<dt>definition list dt</dt>
-<dd>definition list dd</dd>
-<dt>definition list dt</dt>
-<dd>definition list dd</dd>
-<dt>Lorem ipsum dolor sit amet, consectetur adipisicing elit adipisicing elit adipisicing elit</dt>
-<dd>Lorem ipsum dolor sit amet, consectetur adipisicing elit adipisicing elit adipisicing elit</dd>
-<dt>Lorem ipsum dolor sit amet, consectetur adipisicing elit adipisicing elit adipisicing elit</dt>
-<dd>Lorem ipsum dolor sit amet, consectetur adipisicing elit adipisicing elit adipisicing elit</dd>
+  <dt>definition list dt</dt>
+  <dd>definition list dd</dd>
+  <dt>definition list dt</dt>
+  <dd>definition list dd</dd>
+  <dt>Lorem ipsum dolor sit amet, consectetur adipisicing elit adipisicing elit adipisicing elit</dt>
+  <dd>Lorem ipsum dolor sit amet, consectetur adipisicing elit adipisicing elit adipisicing elit</dd>
+  <dt>Lorem ipsum dolor sit amet, consectetur adipisicing elit adipisicing elit adipisicing elit</dt>
+  <dd>Lorem ipsum dolor sit amet, consectetur adipisicing elit adipisicing elit adipisicing elit</dd>
 </dl>
 
 <h3>preformated text</h3>
@@ -153,97 +153,97 @@ The <u>u element</u> example
 
 <h3>table 1</h3>
 <table>
-  <caption><em>A standard test table with a caption, tr, td elements</em></caption>
-  <tbody><tr>
-  <th class="span-4">Table Header One</th>
-  <th class="span-4 last">Table Header Two</th>
-  </tr>
-  <tr>
-  <td>TD One</td>
-  <td>TD Two</td>
-  </tr>
-  <tr>
-  <td colspan="2">TD colspan 2</td>
-  </tr>
-</tbody>
+    <caption><em>A standard test table with a caption, tr, td elements</em></caption>
+    <tbody><tr>
+    <th>Table Header One</th>
+    <th>Table Header Two</th>
+    </tr>
+    <tr>
+    <td>TD One</td>
+    <td>TD Two</td>
+    </tr>
+    <tr>
+    <td colspan="2">TD colspan 2</td>
+    </tr>
+  </tbody>
 </table>
 
 <h3>table 2</h3>
 <table>
-<caption><em>A test table with a thead, tfoot, and tbody elements</em></caption>
-<thead>
-<tr>
-  <th class="span-4">Table Header One</th>
-  <th class="span-4 last">Table Header Two</th>
-</tr>
-</thead>
-<tfoot>
-<tr>
-  <td colspan="2">tfoot footer</td>
-</tr>
-</tfoot>
-<tbody>
-<tr>
-  <td>TD One</td>
-  <td>TD Two</td>
-</tr>
-<tr>
-  <td>TD One</td>
-  <td>TD Two</td>
-</tr>
-</tbody>
-<tbody>
-<tr>
-  <td>TD One</td>
-  <td>TD Two</td>
-</tr>
-<tr>
-  <td>TD One</td>
-  <td>TD Two</td>
-</tr>
-</tbody>
+  <caption><em>A test table with a thead, tfoot, and tbody elements</em></caption>
+  <thead>
+  <tr>
+    <th>Table Header One</th>
+    <th>Table Header Two</th>
+  </tr>
+  </thead>
+  <tfoot>
+  <tr>
+    <td colspan="2">tfoot footer</td>
+  </tr>
+  </tfoot>
+  <tbody>
+  <tr>
+    <td>TD One</td>
+    <td>TD Two</td>
+  </tr>
+  <tr>
+    <td>TD One</td>
+    <td>TD Two</td>
+  </tr>
+  </tbody>
+  <tbody>
+  <tr>
+    <td>TD One</td>
+    <td>TD Two</td>
+  </tr>
+  <tr>
+    <td>TD One</td>
+    <td>TD Two</td>
+  </tr>
+  </tbody>
 </table>
 
 <h3>Table 3</h3>
 <p>Table with a class of table and radius.
 <table class="table radius">
-<caption>Table Caption</caption>
-<colgroup>
-  <col class="two">
-  <col class="ten">
-</colgroup>
-<thead>
-  <tr>
-    <th>Table Header</th>
-    <th>Table Header</th>
-  </tr>
-</thead>
-<tbody>
-  <tr>
-    <td>Content</td>
-    <td>This is longer content</td>
-  </tr>
-  <tr>
-    <td>Content</td>
-    <td>This is longer content</td>
-  </tr>
-  <tr>
-    <td>Content</td>
-    <td>This is longer content</td>
-  </tr>
-  <tr>
-    <td>Content</td>
-    <td>This is longer content</td>
-  </tr>
-  <tr>
-    <td>Content</td>
-    <td>This is longer content</td>
-  </tr>
-</tbody>
-<tfoot>
-  <td>Table Footer</td>
-  <td>Table Footer</td>
-</tfoot>
+  <caption>Table Caption</caption>
+  <colgroup>
+    <col>
+    <col>
+  </colgroup>
+  <thead>
+    <tr>
+      <th>Table Header</th>
+      <th>Table Header</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Content</td>
+      <td>This is longer content</td>
+    </tr>
+    <tr>
+      <td>Content</td>
+      <td>This is longer content</td>
+    </tr>
+    <tr>
+      <td>Content</td>
+      <td>This is longer content</td>
+    </tr>
+    <tr>
+      <td>Content</td>
+      <td>This is longer content</td>
+    </tr>
+    <tr>
+      <td>Content</td>
+      <td>This is longer content</td>
+    </tr>
+  </tbody>
+  <tfoot>
+    <td>Table Footer</td>
+    <td>Table Footer</td>
+  </tfoot>
 </table>
 <!-- End Typography -->
 
@@ -251,69 +251,69 @@ The <u>u element</u> example
 <hr>
 
 <form>
-<fieldset class="fieldset">
-  <legend>Example of a form with a class of fieldset.</legend>
-    <label>Address</label>
-    <input type="text" placeholder="Street">
-    <input type="text" placeholder="City">
-    <input type="text" placeholder="State">
-    <input type="text" placeholder="ZIP">
-</fieldset>
+  <fieldset class="fieldset">
+    <legend>Example of a form with a class of fieldset.</legend>
+      <label>Address</label>
+      <input type="text" placeholder="Street">
+      <input type="text" placeholder="City">
+      <input type="text" placeholder="State">
+      <input type="text" placeholder="ZIP">
+  </fieldset>
 </form>
 
 <form>
 
-<fieldset> 
-  <legend>Inputs as descendents of labels (form legend)</legend>
-  <label>Search</label> <input type="search" name="search5" placeholder="search">
-  <label>Email</label> <input type="email" name="email5" placeholder="email address">
-  <label>Url</label> <input type="url" name="url5" placeholder="website address">
-  <label>Telephone</label> <input type="tel" name="tel5" placeholder="telephone number">
-  <label>Text input <input type="text" value="default value"></label> 
-  <label>Date input <input type="date" value="default value"></label>
-  <label>Date Time input <input type="datetime" value="default value"></label>             
-  <label>Time input <input type="time" value="default value"></label>              
-  <label>Radio input <input type="radio" name="rad"></label> 
-  <label>Checkbox input <input type="checkbox"></label> 
-  <label><input type="radio" name="rad"> Radio input</label> 
-  <label><input type="checkbox"> Checkbox input</label> 
-  <label>Password input <input type="password" value="password"></label> 
-  <label>Select field <select><option>Option 01</option><option>Option 02</option></select></label> 
-  <label>Textarea <textarea cols="30" rows="5">Textarea text</textarea></label> 
-  <select multiple="multiple" size="7" name="input_5[]" id="input_1_5">
-    <option value="First Choice">First Choice</option>
-    <option value="Second Choice">Second Choice</option>
-    <option value="Third Choice">Third Choice</option>
-  </select>
-</fieldset>           
-
-<fieldset> 
-  <p><label>File input <input type="file"></label>
-</fieldset>
-
-
-<fieldset> 
-  <legend>Clickable inputs and buttons with a class of button added</legend>
-  <p><input class="button" type="reset" value="Reset (input)"> 
-  <p><input class="button" type="button" value="Button (input)"> 
-  <p><input class="button" type="submit" value="Submit (input)"> 
-       
-  <p><button class="button" type="reset">Reset (button)</button> 
-  <p><button class="button" type="button">Button (button)</button> 
-  <p><button class="button" type="submit">Submit (button)</button>
-
-  <p>An anchor with class button added <a class="button" href="#">submit</a> 
-</fieldset>
+  <fieldset> 
+    <legend>Inputs as descendents of labels (form legend)</legend>
+    <label>Search</label> <input type="search" name="search5" placeholder="search">
+    <label>Email</label> <input type="email" name="email5" placeholder="email address">
+    <label>Url</label> <input type="url" name="url5" placeholder="website address">
+    <label>Telephone</label> <input type="tel" name="tel5" placeholder="telephone number">
+    <label>Text input <input type="text" value="default value"></label> 
+    <label>Date input <input type="date" value="default value"></label>
+    <label>Date Time input <input type="datetime" value="default value"></label>             
+    <label>Time input <input type="time" value="default value"></label>              
+    <label>Radio input <input type="radio" name="rad"></label> 
+    <label>Checkbox input <input type="checkbox"></label> 
+    <label><input type="radio" name="rad"> Radio input</label> 
+    <label><input type="checkbox"> Checkbox input</label> 
+    <label>Password input <input type="password" value="password"></label> 
+    <label>Select field <select><option>Option 01</option><option>Option 02</option></select></label> 
+    <label>Textarea <textarea cols="30" rows="5">Textarea text</textarea></label> 
+    <select multiple="multiple" size="7" name="input_5[]" id="input_1_5">
+      <option value="First Choice">First Choice</option>
+      <option value="Second Choice">Second Choice</option>
+      <option value="Third Choice">Third Choice</option>
+    </select>
+  </fieldset>           
   
+  <fieldset> 
+    <p><label>File input <input type="file"></label>
+  </fieldset>
+
+
+  <fieldset> 
+    <legend>Clickable inputs and buttons with a class of button added</legend>
+    <p><input class="button" type="reset" value="Reset (input)"> 
+    <p><input class="button" type="button" value="Button (input)"> 
+    <p><input class="button" type="submit" value="Submit (input)"> 
+         
+    <p><button class="button" type="reset">Reset (button)</button> 
+    <p><button class="button" type="button">Button (button)</button> 
+    <p><button class="button" type="submit">Submit (button)</button>
+
+    <p>An anchor with class button added <a class="button" href="#">submit</a> 
+  </fieldset>
+    
 </form>
 
 <h2>Fluid Videos</h2>
 <hr>
 
 <div class="flex-video widescreen">
-<iframe src="http://www.youtube.com/embed/YE7VzlLtp-4?wmode=opaque" frameborder="0" width="560" height="315" allowfullscreen></iframe>
+  <iframe src="http://www.youtube.com/embed/YE7VzlLtp-4?wmode=opaque" frameborder="0" width="560" height="315" allowfullscreen></iframe>
 </div>
 
 <div class="flex-video vimeo widescreen">
-<iframe src="http://player.vimeo.com/video/6284199?title=0&amp;byline=0&amp;portrait=0;wmode=opaque;" frameborder="0" width="560" height="315" webkitAllowFullScreen allowFullScreen></iframe>
+  <iframe src="http://player.vimeo.com/video/6284199?title=0&amp;byline=0&amp;portrait=0;wmode=opaque;" frameborder="0" width="560" height="315" webkitAllowFullScreen allowFullScreen></iframe>
 </div>
--- templates-page/page-grid.php
+++ templates-page/page-grid.php
@@ -4,6 +4,57 @@ Template Name: Grid
 */
 ?>
 
+<style>
+
+  /* Responsive tests
+  ------------------------- */
+  .responsive-utilities-test {
+    margin-top: 5px;
+    padding-left: 0;
+    list-style: none;
+    overflow: hidden; /* clear floats */
+  }
+  .responsive-utilities-test li {
+    position: relative;
+    float: left;
+    width: 16.6666666666667%;
+    height: 43px;
+    font-size: 14px;
+    font-weight: bold;
+    line-height: 43px;
+    color: #999;
+    text-align: center;
+    border: 1px solid #ddd;
+    border-left: none;
+  }
+  .responsive-utilities-test li:first-child {
+    border-left: 1px solid #ddd;
+  }
+  .responsive-utilities-test span {
+    position: absolute;
+    top:    -1px;
+    left:   -1px;
+    right:  -1px;
+    bottom: -1px;
+  }
+  .responsive-utilities-test span {
+    color: #468847;
+    background-color: #dff0d8;
+    border: 1px solid #C1D3B1;
+  }
+
+  .responsive-vis-list dd {
+    margin: 0;
+  }
+  .responsive-vis-list dd ol {
+    list-style: none;
+    padding: 0;
+  }
+  .responsive-vis-list li {
+    display: inline-block;
+  }
+</style>
+
 <header>
   <hgroup>
     <h1>The Grid</h1>
@@ -13,19 +64,59 @@ Template Name: Grid
 
 <p class="h4">The grid is used for dividing space fractionaly and can be nested.</p>
 
-<hr>
-
 <h2>Grid Classes</h2>
 <hr>
 
 <dl>
-  <dt><code>row</code></dt>
-  <dd>Contains the columns.</dd>
-  <dt><code>column</code></dt>
-  <dd>Base class for making columns. Contains float, padding and min-height to prevent empty columns from collapsing. By default columns are set to stack when the screen width is below 768 pixels.</dd>
-  <dt><code>widthXofY</code></dt>
-  <dd>Used in conjuction with the column class. Defines the width of the column. Columns can be thirds, quarters, fifths, halves or full. Total columns in a row must equal 1, for example &frac13; plus &frac23; equals 1.</dd>
+  <dt><code>.row</code></dt>
+  <dd>Contains the columns. The outermost rows contain padding. Nested rows have the padding removed and negative left and right margin added for grid alignment.</dd>
+  <dt><code>.nocolumns</code></dt>
+  <dd>Extension for the row class for outermost rows that contain no columns. It doubles the padding of the row to ensure proper grid alignment.</dd>
+  <dt><code>.fullwidth</code></dt>
+  <dd>Extension for the row class for rows that contain no columns and need to be full width. It removes the padding and negative margin for nested rows.</dd>
+  <dt><code>.column</code></dt>
+  <dd>Base class for making columns. Contains float and padding. By default columns are set to stack when the screen width is below 768 pixels.</dd>
+  <dt><code>.widthXofY</code></dt>
+  <dd>Extension for the column class and can be used independently if desired. Widths can be thirds, quarters, fifths, halves or full. Total columns in a row must equal 1, for example &frac13; plus &frac23; equals 1.</dd>
 </dl>
+
+<h2>Responsive Visibility Classes</h2>
+<hr>
+<p>While these are not specific to the grid, these are helpful for quickly showing and hiding elements on a page. These are not meant for creating entirely different page layouts and should be used sparingly.</p>
+<p>Each class name is associated with the query width and up from that, so you could have an element with the classes <code>.show768.hide0</code> to hide on all but the 768 query width or you could use <code>.show768</code> to show an element from 768 and up.</p>
+<dl class="responsive-vis-list">
+  <dt>Class Names</dt>
+  <dd>
+    <ol>
+      <li><code>.hide0</code> <code>.show0</code></li>
+      <li><code>.hide480</code> <code>.show480</code></li>
+      <li><code>.hide768</code> <code>.show768</code></li>
+      <li><code>.hide800</code> <code>.show800</code></li>
+      <li><code>.hide992</code> <code>.show992</code></li>
+      <li><code>.hide1382</code> <code>.show1382</code></li>
+    </ol>
+  </dd>
+</dl>
+
+<h3>Visible on&hellip;</h3>
+<ol class="responsive-utilities-test">
+  <li>0<span class="">✔ 0</span></li>
+  <li>480<span class="show480 hide0">✔ 480</span></li>
+  <li>600<span class="show600 hide0">✔ 600</span></li>
+  <li>768<span class="show768 hide0">✔ 768</span></li>
+  <li>992<span class="show992 hide0">✔ 992</span></li>
+  <li>1382<span class="show1382 hide0">✔ 1382</span></li>
+</ol>
+
+<h3>Hidden on&hellip;</h3>
+<ol class="responsive-utilities-test hidden-on">
+  <li>0<span class="hide0">✔ 0</span></li>
+  <li>480<span class="hide480">✔ 480</span></li>
+  <li>600<span class="hide600">✔ 600</span></li>
+  <li>768<span class="hide768">✔ 768</span></li>
+  <li>992<span class="hide992">✔ 992</span></li>
+  <li>1382<span class="hide1382">✔ 1382</span></li>
+</ol>
 
 <h2>Grid Examples</h2>
 <hr>
@@ -122,8 +213,8 @@ Template Name: Grid
     <h4>1/4</h4>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum consectetur quam sed sapien bibendum sit amet sodales arcu accumsan. Nam fringilla vulputate tincidunt. Mauris sed sem ante, at luctus justo. Fusce et tempor nunc.</p>
   </div>
-  <div class="column width1of2">
-    <h4>1/2</h4>
+  <div class="column width2of4">
+    <h4>2/4</h4>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum consectetur quam sed sapien bibendum sit amet sodales arcu accumsan. Nam fringilla vulputate tincidunt. Mauris sed sem ante, at luctus justo. Fusce et tempor nunc. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla pharetra nisl nec tortor convallis rutrum vestibulum felis consectetur. Nullam mi nisi, tincidunt id ornare ut, auctor non mi. Cras sagittis bibendum venenatis. In hac habitasse platea dictumst. Nulla in velit orci, quis venenatis lacus.</p>
   </div>
   <div class="column width1of4">
@@ -202,6 +293,8 @@ Template Name: Grid
 
 <h2>Grid Nesting</h2>
 <hr>
+
+<pre><ol><li>&lt;div class="row"&gt;</li><li>  &lt;div class="column width2of3"&gt;</li><li>    &lt;div class="row"&gt;</li><li>      &lt;div class="column width1of2"&gt;&hellip;&lt;/div&gt;</li><li>      &lt;div class="column width1of2"&gt;&hellip;&lt;/div&gt;</li><li>    &lt;/div&gt;</li><li>  &lt;/div&gt;</li><li>  &lt;div class="column width1of3"&gt;&hellip;&lt;/div&gt;</li><li>&lt;/div&gt;</li></ol></pre>
 
 <div class="row">
   <div class="column width2of3">
