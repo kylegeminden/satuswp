fc1e06b961c5b5542565023d9f7f7180aa1f1c9b
Kyle Geminden <kyle@kylegeminden.com>
2013-03-28 19:25:54 -0600

    added class to search input, changed submit button to button for versatility

--- templates/searchform.php
+++ templates/searchform.php
@@ -1,5 +1,5 @@
 <form class="search-form" role="search" method="get" action="<?php echo home_url( '/' ); ?>">
   <label for="s">Search for:</label>
-  <input type="search" id="s" name="s" placeholder="Search" value="<?php the_search_query(); ?>">
-  <input class="button" type="submit" value="<?php _e('Go', 'satus');?>">
+  <input type="search" id="s" name="s" class="search-query" placeholder="Search" value="<?php the_search_query(); ?>">
+  <button class="button"><?php _e('Go', 'satus');?></button>
 </form>
