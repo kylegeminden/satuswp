1aabbb1b8ffbb9680f20457e3f561d6d40573b8c
Kyle Geminden <kyle@kylegeminden.com>
2013-05-17 19:08:20 -0600

    Added class to description and updated posts nav.

--- category.php
+++ category.php
@@ -3,13 +3,11 @@
 <?php
   $category_description = category_description();
   if ( ! empty( $category_description ) )
-  echo apply_filters( 'category_archive_meta', '<div itemprop="description">' . $category_description . '</div>' );
+  echo apply_filters( 'category_archive_meta', '<div class="category-description" itemprop="description">' . $category_description . '</div>' );
 ?>
 
 <?php while ( have_posts() ) : the_post(); ?>
 <?php get_template_part( 'templates/content' ); ?>
 <?php endwhile; ?>
 
-<?php if ( $wp_query->max_num_pages > 1 ) : ?>
-  <?php satus_posts_nav(POSTS_NAV_PREV, POSTS_NAV_NEXT); ?>
-<?php endif; ?>
+<?php satus_posts_nav(POSTS_NAV_PREV, POSTS_NAV_NEXT); ?>
--- tag.php
+++ tag.php
@@ -3,13 +3,11 @@
 <?php
   $tag_description = tag_description();
   if ( ! empty( $tag_description ) )
-    echo apply_filters( 'tag_archive_meta', '<div itemprop="description">' . $tag_description . '</div>' );
+    echo apply_filters( 'tag_archive_meta', '<div class="tag-description" itemprop="description">' . $tag_description . '</div>' );
 ?>
 
 <?php while ( have_posts() ) : the_post(); ?>
 <?php get_template_part( 'templates/content' ); ?>
 <?php endwhile; ?>
 
-<?php if ( $wp_query->max_num_pages > 1 ) : ?>
-  <?php satus_posts_nav(POSTS_NAV_PREV, POSTS_NAV_NEXT); ?>
-<?php endif; ?>
+<?php satus_posts_nav(POSTS_NAV_PREV, POSTS_NAV_NEXT); ?>
