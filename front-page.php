fbcfecc85c8c3fc27ed3aa4e2e3ef0518eb6e23f
Kyle Geminden <kyle@kylegeminden.com>
2013-05-17 19:09:03 -0600

    Updated posts nav.

--- front-page.php
+++ front-page.php
@@ -10,8 +10,6 @@
   <?php get_template_part( 'templates/content' ); ?>
   <?php endwhile; ?>
 
-  <?php if ( $wp_query->max_num_pages > 1 ) : ?>
-    <?php satus_posts_nav(POSTS_NAV_PREV, POSTS_NAV_NEXT); ?>
-  <?php endif; ?>
+  <?php satus_posts_nav(POSTS_NAV_PREV, POSTS_NAV_NEXT); ?>
 
 <?php endif; ?>
