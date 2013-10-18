65c553f5574aa0d339052b6ba6eebae11f63ac70
Kyle Geminden <kyle@kylegeminden.com>
2013-10-17 19:01:46 -0600

    Removed reference to walker class in `header.php`.

--- templates/header.php
+++ templates/header.php
@@ -1,14 +1,14 @@
-<header class="header" role="banner">  
+<header class="header" role="banner">
   <div class="row">
     <div class="column width1of1">
-      
+
       <div class="brand">
         <h1 class="site-title">
           <a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
         </h1>
         <p class="h2 site-description"><?php bloginfo( 'description' ); ?></p>
       </div>
-      
+
       <nav class="primary-nav" role="navigation">
         <h2 class="visuallyhidden"><?php _e( 'Primary Navigation', 'satus' ); ?></h2>
         <a class="visuallyhidden" href="#main" title="<?php _e('Go to main content', 'satus') ?>"><?php _e('Go to main content', 'satus') ?></a>
@@ -18,8 +18,9 @@
               'theme_location' => 'primary_nav',
               'depth' => 2,
               'container' => false,
-              'items_wrap' => '<ul class="nav">%3$s</ul>',
-              'walker' => new Satus_Navbar_Walker()
+              'items_wrap' => '<ul class="nav">%3$s</ul>'
+              // ,
+              // 'walker' => new Satus_Navbar_Walker()
             ));
           }
         ?>
