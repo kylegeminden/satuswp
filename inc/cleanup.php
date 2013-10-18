f8e43099f007471b310460c2e7e9f50d032ed2ce
Kyle Geminden <kyle@kylegeminden.com>
2013-10-17 19:01:21 -0600

    Removed `satus_get_search_form()`, `Satus_Nav_Walker` and `Satus_Navbar_Waker` function from `cleanup.php`.

--- inc/cleanup.php
+++ inc/cleanup.php
@@ -26,15 +26,6 @@ function satus_change_mce_options($options) {
 }
 add_filter('tiny_mce_before_init', 'satus_change_mce_options');
 
-/** 
- * Tell WordPress to use searchform.php from the templates/ directory
- * @link https://github.com/retlehs/roots/
- */
-function satus_get_search_form() {
-  locate_template('/templates/searchform.php', true, true);
-}
-add_filter('get_search_form', 'satus_get_search_form');
-
 /**
  * Redirects search results from /?s=query to /search/query/, converts %20 to +
  *
@@ -92,7 +83,7 @@ remove_action('wp_head', 'wp_generator');
  */
 add_filter('the_generator', '__return_false');
 
-/** 
+/**
  * Add and remove body_class() classes
  * @link https://github.com/retlehs/roots/
  */
@@ -278,129 +269,6 @@ function satus_gallery($attr) {
 if (GALLERY) {
   remove_shortcode('gallery');
   add_shortcode('gallery', 'satus_gallery');
-}
-
-/**
- * Custom Walker that adds menu-item-slug (menu item title) and description
- */
-class Satus_Nav_Walker extends Walker_Nav_Menu {
-  function start_el(&$output, $item, $depth, $args) {
-    global $wp_query;
-      $indent = ( $depth ) ? str_repeat( "\t", $depth ) : '';     
-      $slug = sanitize_title($item->title);
-      $class_names = $value = '';
-      $classes = empty( $item->classes ) ? array() : (array) $item->classes;
-      $classes[] = 'menu-item-' . $item->ID;
-      $class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args ) );
-      $class_names = ' class="' . 'menu-item-' . $slug . ' ' . esc_attr( $class_names ) . '"';
-      $id = apply_filters( 'nav_menu_item_id', 'menu-item-'. $item->ID, $item, $args );
-      $id = strlen( $id ) ? ' id="' . esc_attr( $id ) . '"' : '';
-      $output .= $indent . '<li' . $id . $value . $class_names .'>';
-      $attributes  = ! empty( $item->attr_title ) ? ' title="'  . esc_attr( $item->attr_title ) .'"' : '';
-      $attributes .= ! empty( $item->target )     ? ' target="' . esc_attr( $item->target     ) .'"' : '';
-      $attributes .= ! empty( $item->xfn )        ? ' rel="'    . esc_attr( $item->xfn        ) .'"' : '';
-      $attributes .= ! empty( $item->url )        ? ' href="'   . esc_attr( $item->url        ) .'"' : '';
-      
-      $prepend = '<span class="menu-item-label">';
-      $append = '</span>';
-      $description  = ! empty( $item->description ) ? '<span class="menu-item-dscrp">'.esc_attr( $item->description ).'</span>' : '';
-      if($depth != 0)
-      {
-        $description = $append = $prepend = "";
-      }
-      $item_output = $args->before;
-      $item_output .= '<a'. $attributes .'>';
-      $item_output .= $args->link_before .$prepend.apply_filters( 'the_title', $item->title, $item->ID ).$append;
-      $item_output .= $description.$args->link_after;
-
-      $item_output .= '</a>';
-      $item_output .= $args->after;
-      $output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
-  }
-}
-
-/**
- * Custom walker class for the primary navigation that adds menu-item-slug (menu item title), description and adds items needed for drop down menu
- * inspired from @link https://github.com/retlehs/roots/
- */
-class Satus_Navbar_Walker extends Walker_Nav_Menu {
-  function start_lvl(&$output, $depth) {
-    $output .= "\n<ul class=\"dropdown-menu\">\n";
-  }
-  function start_el(&$output, $item, $depth, $args) {
-    global $wp_query;
-    $indent = ($depth) ? str_repeat("\t", $depth) : '';
-    $slug = sanitize_title($item->title);
-    $li_attributes = '';
-    $class_names = $value = '';
-    $classes = empty( $item->classes ) ? array() : (array) $item->classes;
-    $classes[] = 'menu-item-' . $item->ID;    
-    if ($args->has_children) {
-      $classes[]      = 'dropdown';
-      // $li_attributes .= ' data-dropdown="dropdown"';
-    }
-    $classes[] = ($item->current) ? 'active' : '';    
-    $class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args ) );
-    $class_names = ' class="' . 'menu-item-' . $slug . ' ' . esc_attr( $class_names ) . '"';    
-    $id = apply_filters( 'nav_menu_item_id', 'menu-item-'. $item->ID, $item, $args );
-    $id = strlen( $id ) ? ' id="' . esc_attr( $id ) . '"' : '';
-    $output .= $indent . '<li' . $id . $value . $class_names . $li_attributes . '>';
-    $attributes  = ! empty($item->attr_title) ? ' title="'  . esc_attr($item->attr_title) .'"'    : '';
-    $attributes .= ! empty($item->target)     ? ' target="' . esc_attr($item->target    ) .'"'    : '';
-    $attributes .= ! empty($item->xfn)        ? ' rel="'    . esc_attr($item->xfn       ) .'"'    : '';
-    $attributes .= ! empty($item->url)        ? ' href="'   . esc_attr($item->url       ) .'"'    : '';
-    $attributes .= ($args->has_children)      ? ' class="dropdown-toggle" data-toggle="dropdown"' : '';
-
-    $prepend = '<span class="menu-item-label">';
-    $append = '</span>';
-    $description  = ! empty( $item->description ) ? '<span class="menu-item-dscrp">'.esc_attr( $item->description ).'</span>' : '';
-    if($depth != 0)
-    {
-      $description = $append = $prepend = "";
-    }
-    $item_output = $args->before;
-    $item_output .= '<a'. $attributes .'>';
-    $item_output .= $args->link_before .$prepend.apply_filters( 'the_title', $item->title, $item->ID ).$append;
-    $item_output .= $description.$args->link_after;
-    $item_output .= ($args->has_children) ? ' <b class="caret"></b>' : '';
-    $item_output .= '</a>';
-    $item_output .= $args->after;
-    $output .= apply_filters('walker_nav_menu_start_el', $item_output, $item, $depth, $args);
-  }
-  function display_element($element, &$children_elements, $max_depth, $depth = 0, $args, &$output) {
-    if (!$element) { return; }
-    $id_field = $this->db_fields['id'];
-    // display this element
-    if (is_array($args[0])) {
-      $args[0]['has_children'] = !empty($children_elements[$element->$id_field]);
-    } elseif (is_object($args[0])) {
-      $args[0]->has_children = !empty($children_elements[$element->$id_field]);
-    }
-    $cb_args = array_merge(array(&$output, $element, $depth), $args);
-    call_user_func_array(array(&$this, 'start_el'), $cb_args);
-    $id = $element->$id_field;
-    // descend only when the depth is right and there are childrens for this element
-    if (($max_depth == 0 || $max_depth > $depth+1) && isset($children_elements[$id])) {
-      foreach ($children_elements[$id] as $child) {
-        if (!isset($newlevel)) {
-          $newlevel = true;
-          // start the child delimiter
-          $cb_args = array_merge(array(&$output, $depth), $args);
-          call_user_func_array(array(&$this, 'start_lvl'), $cb_args);
-        }
-        $this->display_element($child, $children_elements, $max_depth, $depth + 1, $args, $output);
-      }
-      unset($children_elements[$id]);
-    }
-    if (isset($newlevel) && $newlevel) {
-      // end the child delimiter
-      $cb_args = array_merge(array(&$output, $depth), $args);
-      call_user_func_array(array(&$this, 'end_lvl'), $cb_args);
-    }
-    // end this element
-    $cb_args = array_merge(array(&$output, $element, $depth), $args);
-    call_user_func_array(array(&$this, 'end_el'), $cb_args);
-  }
 }
 
 ?>
